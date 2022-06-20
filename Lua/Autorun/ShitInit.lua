f2b14dd1a9b854d0aaa0247721763ff9714709b9
iller_saver <illersaver@gmail.com>
2022-06-21 01:55:25 +0300

    Cool serverside checking
    yeah

--- Lua/Autorun/ShitInit.lua
+++ Lua/Autorun/ShitInit.lua
@@ -1,6 +1,9 @@
---Cool sign
-print("\n=-=-=-=-=-=-=-=-=-=-=-=\n          Ilya's shit 0.0.3 \n                   by Ilya \n=-=-=-=-=-=-=-=-=-=-=-= \n smite \n secretpanel\n")
---Cool shit
-    require("Funny Shit.Smite")
-    require("Funny Shit.SecretPanel")
-    require("Funny Shit.FunctionTest")
+if CLIENT then return end
+if SERVER then
+    --Cool sign
+    print("\n=-=-=-=-=-=-=-=-=-=-=-=\n          Ilya's shit 0.0.3 \n                   by Ilya \n=-=-=-=-=-=-=-=-=-=-=-= \n smite \n secretpanel\n")
+    --Cool shit
+        require("Funny Shit.Smite")
+        require("Funny Shit.SecretPanel")
+        require("Funny Shit.FunctionTest")
+end
