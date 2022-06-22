60b6f47ee6aa101dd0d7ada739ab3a72c83c20b6
iller_saver <illersaver@gmail.com>
2022-06-22 15:54:34 +0300

    List command

--- Lua/Autorun/ShitInit.lua
+++ Lua/Autorun/ShitInit.lua
@@ -1,7 +1,11 @@
 if CLIENT then return end
 if SERVER then
     --Cool sign
-    print("\n=-=-=-=-=-=-=-=-=-=-=-=\n          Ilya's shit 0.0.3 \n                   by Ilya \n=-=-=-=-=-=-=-=-=-=-=-= \n Type (''command'' Help), if you need too understand what it does\n smite \n antismite \n secretpanel\n affliction")
+    print("\n=-=-=-=-=-=-=-=-=-=-=-=\n          Ilya's shit 0.0.3 \n                   by Ilya \n=-=-=-=-=-=-=-=-=-=-=-= \n Type (''command'' Help), if you need too understand what it does\n list \n smite \n antismite \n secretpanel\n affliction")
+    Game.AddCommand("list", "hee-hee-hi-ha", function () end, getValidArgs, isCheat)
+    Game.AssignOnClientRequestExecute("list", function(client,cursor,args)
+        print("('command' Help) for info\n list \n smite \n antismite \n secretpanel\n affliction")
+    end)
     --Cool shit
         require("Funny Shit.Smite")
         require("Funny Shit.SecretPanel")
